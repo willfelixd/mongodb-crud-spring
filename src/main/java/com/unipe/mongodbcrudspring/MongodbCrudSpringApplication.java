c7f7e7635b664cbf189090f3e5857bbf0c14bfb9
willfelixd <willfelixd@gmail.com>
2024-08-15 23:46:09 -0300

    Primeiro commit

---
+++ src/main/java/com/unipe/mongodbcrudspring/MongodbCrudSpringApplication.java
@@ -0,0 +1,13 @@
+package com.unipe.mongodbcrudspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbCrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbCrudSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unipe/mongodbcrudspring/MongodbCrudSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unipe.mongodbcrudspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbCrudSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
