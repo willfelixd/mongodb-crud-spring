fecf3143c1f5a9a2a261720707b931ae699ebc61
willfelixd <willfelixd@gmail.com>
2024-08-16 03:04:47 -0300

    Controller adicionado

---
+++ src/main/java/com/unipe/mongodbcrudspring/api/UsuarioController.java
@@ -0,0 +1,35 @@
+package com.unipe.mongodbcrudspring.api;
+
+import com.unipe.mongodbcrudspring.api.request.UsuarioRequestDTO;
+import com.unipe.mongodbcrudspring.api.response.UsuarioResponseDTO;
+import com.unipe.mongodbcrudspring.business.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @PostMapping()
+    public ResponseEntity<UsuarioResponseDTO> gravaDadosUsuario(@RequestBody UsuarioRequestDTO usuarioRequestDTO) {
+        return ResponseEntity.ok(usuarioService.gravarUsuarios(usuarioRequestDTO));
+    }
+
+
+    @GetMapping()
+    public ResponseEntity<UsuarioResponseDTO> buscaUsuarioPorEmail(@RequestParam ("email") String email) {
+        return ResponseEntity.ok(usuarioService.buscaDadosUsuario(email));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletaDadosUsuario(@RequestParam ("email") String email) {
+        usuarioService.deletaDadosUsuario(email);
+        return ResponseEntity.accepted().build();
+    }
+
+}
