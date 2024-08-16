14a6b7dd121fd41379ffe3e0b88b70f675596549
willfelixd <willfelixd@gmail.com>
2024-08-16 02:04:24 -0300

    Métodos delete adicionados

--- src/main/java/com/unipe/mongodbcrudspring/infrastructure/repository/EnderecoRepository.java
+++ src/main/java/com/unipe/mongodbcrudspring/infrastructure/repository/EnderecoRepository.java
@@ -2,6 +2,12 @@ package com.unipe.mongodbcrudspring.infrastructure.repository;
 
 import com.unipe.mongodbcrudspring.infrastructure.entity.EnderecoEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface EnderecoRepository extends MongoRepository<EnderecoEntity, String> {
+
+    EnderecoEntity findByUsuarioId(String usuarioId);
+
+    @Transactional
+    void deleteByUsuarioId(String usuarioId);
 }
--- src/main/java/com/unipe/mongodbcrudspring/infrastructure/repository/UsuarioRepository.java
+++ src/main/java/com/unipe/mongodbcrudspring/infrastructure/repository/UsuarioRepository.java
@@ -2,8 +2,14 @@ package com.unipe.mongodbcrudspring.infrastructure.repository;
 
 import com.unipe.mongodbcrudspring.infrastructure.entity.UsuarioEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 
 public interface UsuarioRepository extends MongoRepository<UsuarioEntity, String> {
+
+    UsuarioEntity findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
 
 }
