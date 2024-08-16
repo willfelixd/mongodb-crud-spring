8922ab5da461b84ce0022d7875415a8fa52da3c4
willfelixd <willfelixd@gmail.com>
2024-08-15 23:55:09 -0300

    Entidades criadas

--- src/main/java/com/unipe/mongodbcrudspring/MongodbCrudSpringApplication.java
+++ src/main/java/com/unipe/mongodbcrudspring/MongodbCrudSpringApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MongodbCrudSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MongodbCrudSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbCrudSpringApplication.class, args);
+    }
 
 }
--- src/test/java/com/unipe/mongodbcrudspring/MongodbCrudSpringApplicationTests.java
+++ src/test/java/com/unipe/mongodbcrudspring/MongodbCrudSpringApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MongodbCrudSpringApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
